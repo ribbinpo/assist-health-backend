9c66c10a7fb26bab7e57e62bd6be5800ba784974
sample <s6230613032@phuket.psu.ac.th>
2022-11-23 09:37:47 +0700

    booking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { BookingService } from './booking.service';
 
 @Controller('booking')
